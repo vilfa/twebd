6b5d11ec5ade7a91efba2bbc9e598af8d3bd09a2
Luka Vilfan <luka.vilfan@protonmail.com>
2021-09-19 20:46:06 +0200

    test signed commits

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use twebd::cli::runner;
 // DONE - implement a message queue for log messages for when the logger is uninitialized;
 // DONE - modify all http structs to use Option<T>, to enable universal request and response usage;
 // DONE - implement simple http requests and responses;
+// DONE - generate self-signed certificates for tls testing;
 // - implement https module;
 
 fn main() {
